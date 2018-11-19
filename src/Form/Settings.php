2e9f6cc5d69f3a268be7382521089ae48153304e
dakku <dakku@97634.no-reply.drupal.org>
2018-11-19 19:05:15 +0000

    Remove data cache options

--- src/Form/Settings.php
+++ src/Form/Settings.php
@@ -99,59 +99,25 @@ class Settings extends ConfigFormBase {
         '#type' => 'textfield',
         '#title' => $this->t('Access Token'),
         '#default_value' => $config->get('access_token'),
+        '#disabled' => TRUE,
         '#description' => $this->t('Access Tokens expire in 8 hours. To get your Access Token, @link.',
           [
             '@link' => Link::fromTextAndUrl('click here', Url::fromUri($this->accessUrl(), $options))->toString(),
           ]),
       ];
-    }
 
-    if ($config->get('client_id') != '' && $config->get('client_secret') != '') {
       $form['client']['refresh_token'] = [
         '#type' => 'textfield',
         '#title' => $this->t('Refresh Token'),
-        '#description' => $this->t('Refresh Tokens will expire immediately after being used to obtain new tokens, or after 1 year if they are not used to obtain new tokens'),
+        '#disabled' => TRUE,
+        '#description' => $this->t('Refresh Tokens expire after 1 year if they are not used to obtain new tokens. To get your Refresh Token, @link.',
+          [
+            '@link' => Link::fromTextAndUrl('click here', Url::fromUri($this->accessUrl(), $options))->toString(),
+          ]),
         '#default_value' => $config->get('refresh_token'),
       ];
     }
-
-    $form['caching'] = [
-      '#type' => 'details',
-      '#title' => $this->t('Eloqua API Caching'),
-      '#open' => TRUE,
-      '#description' => $this->t('API caching is recommended for all websites.'),
-    ];
-
-    // Identical options to the ones for block caching.
-    // @see \Drupal\Core\Block\BlockBase::buildConfigurationForm()
-    $period = [
-      0,
-      60,
-      180,
-      300,
-      600,
-      900,
-      1800,
-      2700,
-      3600,
-      10800,
-      21600,
-      32400,
-      43200,
-      86400,
-    ];
-
-    $period = array_map([$this->dateFormatter, 'formatInterval'], array_combine($period, $period));
-    $period[0] = '<' . $this->t('no caching') . '>';
-
-    $form['caching']['api_cache_maximum_age'] = [
-      '#type' => 'select',
-      '#title' => $this->t('API cache maximum age'),
-      '#default_value' => $config->get('api_cache_maximum_age'),
-      '#options' => $period,
-      '#description' => $this->t('The maximum time a API request can be cached by Drupal.'),
-    ];
-
+    
     return parent::buildForm($form, $form_state);
   }
 
@@ -164,7 +130,6 @@ class Settings extends ConfigFormBase {
       ->set('client_secret', $form_state->getValue('client_secret'))
       ->set('access_token', $form_state->getValue('access_token'))
       ->set('refresh_token', $form_state->getValue('refresh_token'))
-      ->set('api_cache_maximum_age', $form_state->getValue('api_cache_maximum_age'))
       ->save();
 
     parent::submitForm($form, $form_state);
